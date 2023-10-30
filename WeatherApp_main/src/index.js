db5a65a7deb0d5bc279d33b2bcbe9524ffa5efff
rsterenchak <rsterenchak@gmail.com>
2023-10-30 01:25:26 +0000

    Finished adding necessary set up for npm/webpack

--- WeatherApp_main/src/index.js
+++ WeatherApp_main/src/index.js
@@ -1,10 +1,17 @@
 import _ from 'lodash';
+import printMe from './logic.js';
+import './style.css';
+import Icon from './icon.png';
+
+// import Data from './data.xml';
+// import Notes from './data.csv';
 
 function component() {
     const element = document.createElement('div');
   
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
   
     return element;
   }
